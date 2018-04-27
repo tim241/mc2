05df1d13c8fee03122d01cee4bef40269f5c2ad9
Tim Wanders <timwanders241@gmail.com>
2018-04-28 00:30:27 +0200

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 set -e
 _work_dir="$(pwd)"
-[ -d "build" ] && rm -rf build
-mkdir build 
-cd build 
-cmake "$_work_dir" $_cmake_args -DCMAKE_CXX_COMPILER="$(which clang++)" -DCMAKE_CXX_FLAGS="-lSDL2"
+[ -d "build64" ] && rm -rf build
+mkdir build64
+cd build64
+cmake .. $_cmake_args -DCMAKE_CXX_FLAGS="-lSDL2" -DCMAKE_LIBRARY_ARCHITECTURE=x64
 make $_make_args
-
-
+cd "$_work_dir/text_tool"
+cmake . $_cmake_args -DCMAKE_CXX_FLAGS="-lSDL2"
+make $_make_args
+cd "$_work_dir/data_tools"
+cmake . $_cmake_args -DCMAKE_CXX_FLAGS="-lSDL2"
+make $_make_args
